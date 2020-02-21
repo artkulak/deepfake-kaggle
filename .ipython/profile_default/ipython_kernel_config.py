1d3ac95adbd4a244810ee89137e17cc38463264c
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-21 20:00:04 +0000

    Added video extraction script

---
+++ .ipython/profile_default/ipython_kernel_config.py
@@ -0,0 +1,5 @@
+c.IPKernelApp.extensions = [
+    'google.cloud.bigquery',
+    'sql'
+]
+c.InteractiveShellApp.matplotlib = 'inline'
---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,9 @@
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8080
+c.NotebookApp.allow_origin_pat = '(^https://8080-dot-[0-9]+-dot-devshell\.appspot\.com$)|(^https://colab\.research\.google\.com$)|((https?://)?[0-9a-z]+-dot-datalab-vm[\-0-9a-z]*.googleusercontent.com)|((https?://)?[0-9a-z]+-dot-[\-0-9a-z]*.notebooks.googleusercontent.com)'
+c.NotebookApp.allow_remote_access = True
+c.NotebookApp.disable_check_xsrf = False
+c.NotebookApp.notebook_dir = '/home'
+c.NotebookApp.notebook_dir = '/home/jupyter'
